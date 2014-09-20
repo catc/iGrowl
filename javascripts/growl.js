d624a8e7d7d68a6ac50b30e5e43a0fef741860b7
catc <catalin.covic@gmail.com>
2014-09-20 01:49:28 -0400

    fixed hide animation on dismiss

--- javascripts/growl.js
+++ javascripts/growl.js
@@ -126,6 +126,7 @@
 
 			if ( options.animation ) {
 				this.template
+					.addClass( options.animShow )
 					.addClass( options.animHide )
 					.one(animStart, function(e){
 						if ( typeof options.onHide === 'function' ) options.onHide();
