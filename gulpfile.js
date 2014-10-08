3eeb024a3cf4de314e16bf7eeea771c655f0bd9f
Catalin Covic <catalin.covic@gmail.com>
2014-10-07 20:47:42 -0400

    cleaned gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,209 +7,167 @@ app.set('port', process.env.PORT || 3000);
 // gulp modules
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
-	// autoprefixer = require('gulp-autoprefixer'),
 	minifycss = require('gulp-minify-css'),
 	rename = require('gulp-rename'), 
 	concat = require('gulp-concat'),
 	uglify = require('gulp-uglify'),
-	jade = require('gulp-jade')
+	jade = require('gulp-jade'),
+	livereload = require('gulp-livereload');
+
+
+var css = __dirname + '/public/stylesheets',
+	js = __dirname + '/public/javascripts',
+	fonts = __dirname + '/public/fonts';
 
 
 
+// -------------- development --------------
+var dev = {
+	scssInit  : css + '/scss/*.scss', 		// initial scss processing
+	cssDest   : css, 						// css destination
+	scssWatch : css + '/**/*.scss', 		// scss watch
+	cssReload : css + '/*.css', 			// live-reload watch
+};
 
-var cssDef = __dirname + '/public/stylesheets' 
-var css = {
-	// initial path
-	init : cssDef + '/scss/*.scss',
-	// output destination
-	dir : cssDef,
-	// to watch for livereload
-	fin : cssDef + '/*.css',
-	// reload watching
-	watch : cssDef + '/**/*.scss'
-}
-// scss task
-gulp.task('css', function(){
-	return gulp.src( css.init )
-		.pipe( sass({
-			outputStyle: 'nested'
-		}) )
-		// .pipe( autoprefixer('last 2 version') )
-		.pipe( gulp.dest( css.dir ) )
-		
-		// minify
-/*		.pipe( rename({ suffix: '.min' }) )
-		.pipe( minifycss() )
-		.pipe( gulp.dest( css.dir ) )*/
-})
- 
- 
-// livereload
-var tinylr;
-gulp.task('livereload', function(){
-	tinylr = require('tiny-lr')();
-	tinylr.listen(35729)
-})
- 
-function notifyLiveReload(event){
-	var fileName = require('path').relative(__dirname, event.path);
-	
-	tinylr.changed({
-		body: {
-			files: [fileName]
-		}
-	})
-}
- 
-// scripts task
-var scripts = __dirname + '/public/javascripts' 
-gulp.task('scripts', function(){
-	return gulp.src( scripts + '/*.js' )
-		.pipe( concat('main.js') )
-		.pipe( gulp.dest( scripts + '/final/' ) )
- 
-		// minify
-		/*.pipe(rename({ suffix: '.min' }) )
-		.pipe( uglify() )
-		.pipe( gulp.dest( scripts + '/final/' ) )*/
-})
- 
+// compile 
+gulp.task('scss', function(){
+	gulp.src( dev.scssInit )
+		.pipe( sass() )
+		.pipe( gulp.dest( dev.cssDest ) );
+});
+   
 // watch (scss for compile, css for livereload)
 gulp.task('watch', function(){
-	gulp.watch( css.watch, ['css'] )
-	gulp.watch( css.fin, notifyLiveReload )
-})
+	// watch scss
+	gulp.watch( dev.scssWatch, ['scss'] );
+
+	// watch css
+	livereload.listen();
+	gulp.watch( dev.cssReload ).on('change', livereload.changed);
+});
  
 // start express
 gulp.task('express', function(){
 	var server = app.listen(app.get('port'), function() {
 		debug('Express server listening on port ' + server.address().port);
 	});
-})
+});
  
  
-// -------------- GULP DEFAULT --------------
-gulp.task('default', ['css', 'express', 'livereload', 'watch'], function(){
-	console.log('gulp successfully started!')
-})
+gulp.task('dev', ['scss', 'express', 'watch'], function(){
+	console.log('gulp successfully started!');
+});
 
 
 
+// -------------- dist --------------
+var dist = {
+	dest : __dirname + '/dist/',
 
+	cssInit : css + '/igrowl.css',
+	get cssDest (){ return this.dest + '/css'; },
 
+	jsInit : js + '/igrowl.js',
+	get jsDest (){ return this.dest + '/js'; },
 
-// -------------- DISTRIBUTION --------------
-var cssDist = {
-	init : '.public/stylesheets/scss/igrowl.scss',
-	fin : './dist/css/'
-}
-// igrowl scss
-gulp.task('scss-dist', function(){
-	return gulp.src( cssDef + '/scss/igrowl.scss' )
-		.pipe( sass({
-			outputStyle: 'expanded'
-		}) )
-		.pipe( gulp.dest( cssDist.fin ) )
-		
-		// minify
+	fontInit : fonts,
+	get fontDest (){ return this.dest + '/fonts'; }
+};
+
+// igrowl.css + icon font css
+gulp.task('dist-css', function(){
+	// igrowl.css
+	gulp.src( dist.cssInit )
+		.pipe( gulp.dest( dist.cssDest ) )
 		.pipe( rename({ suffix: '.min' }) )
 		.pipe( minifycss() )
-		.pipe( gulp.dest( cssDist.fin ) )
-})
+		.pipe( gulp.dest( dist.cssDest ) );
 
-// icon css
-gulp.task('icon-css-dist', function(){
-	return gulp.src( cssDef + '/icomoon/*.css' )
-		.pipe( gulp.dest( cssDist.fin + '/fonts' ) )
-})
-
+	// icon font css
+	gulp.src( css + '/icomoon/*.css' )
+		.pipe( gulp.dest( dist.cssDest + '/fonts/' ) );
+});
 
 // scripts
-var jsDist = {
-	init : './public/javascripts/igrowl.js',
-	fin : './dist/js/'
-}
-gulp.task('js-dist', function(){
-	return gulp.src( jsDist.init )
-		.pipe( gulp.dest( jsDist.fin ) )
- 
-		// minify
-		.pipe(rename({ suffix: '.min' }) )
-		.pipe( uglify({
-			preserveComments : 'some'
-		}) )
-		.pipe( gulp.dest( jsDist.fin ) )
-})
-
+gulp.task('dist-scripts', function(){
+	gulp.src( dist.jsInit )
+		.pipe( gulp.dest( dist.jsDest ) )
+		.pipe( rename({ suffix: '.min' }) )
+		.pipe( uglify({ preserveComments : 'some' }) )
+		.pipe( gulp.dest( dist.jsDest) );
+});
 
 // fonts
-var fontDist = {
-	init : ['./public/fonts/*/*', '!./public/fonts/interface/**'],
-	fin : './dist/fonts/'
-}
-gulp.task('font-dist', function(){
-	return gulp.src( fontDist.init )
-		.pipe( gulp.dest( fontDist.fin ) )
-})
+gulp.task('dist-fonts', function(){
+	gulp.src( [ dist.fontInit + '/*/*', '!' + dist.fontInit+ '/interface/**'] )
+		.pipe( gulp.dest( dist.fontDest ) );
+});
 
 
-gulp.task('dist', ['scss-dist', 'icon-css-dist', 'js-dist', 'font-dist'], function(){
-	console.log( 'done preparing dist' )
-})
+gulp.task('dist', ['scss', 'dist-css', 'dist-scripts', 'dist-fonts'], function(){
+	console.log( 'successfully set up dist' );
+});
 
 
 
-// -------------- GITHUB PAGE --------------
 
-// jade
-var jadeDist = {
-	init : './views/index.jade', 
-	fin : './public/githubpage/'
-}
-gulp.task('jade-ghp', function(){
-	return gulp.src( jadeDist.init )
+// -------------- github page --------------
+var gh = {
+	dest : __dirname + '/gh-pages/',
+
+	jadeInit : 'views/index.jade',
+
+	get cssDest (){ return this.dest + '/stylesheets/'; },
+	get jsDest (){ return this.dest + '/javascripts/'; },
+	get fontDest (){ return this.dest + '/fonts/'; },
+
+};
+
+// jade to index.html
+gulp.task('gh-jade', function(){
+	gulp.src( gh.jadeInit )
 		.pipe( jade() )
-		.pipe( gulp.dest( jadeDist.fin ) )
-})
+		.pipe( gulp.dest( gh.dest ) );
+});
 
-// concat scripts
-gulp.task('js-ghp', function(){
-	return gulp.src( [
-		'./public/javascripts/jquery.js',
-		'./public/javascripts/*.js', 
-		] )
-		.pipe( concat('all.js') )
-		// .pipe( gulp.dest( scripts + '/final/' ) )
- 
-		// minify
-		.pipe(rename({ suffix: '.min' }) )
+// main css files + icon font css
+gulp.task('gh-css', function(){
+	// main css files
+	gulp.src( css + '/*.css' )
+		.pipe( concat('style.css') )
+		.pipe( minifycss() )
+		.pipe( gulp.dest( gh.cssDest ) );
+
+	// icomoon css fonts
+	gulp.src( css +  '/icomoon/*.css' )
+		.pipe( concat('icons.css') )
+		.pipe( minifycss() )
+		.pipe( gulp.dest( gh.cssDest + '/icomoon/' ) );
+});
+
+// all scripts + animate.json
+gulp.task('gh-scripts', function(){
+	gulp.src([
+			js + '/jquery.js',
+			js + '/*.js'
+		])
+		.pipe( concat('main.js') )
 		.pipe( uglify() )
-		.pipe( gulp.dest( './public/githubpage/' ) )
-})
+		.pipe( gulp.dest( gh.jsDest ) );
 
-// concat main css
-gulp.task('css-ghp1', function(){
-	return gulp.src([ 
-			'./public/stylesheets/*.css',
-		])
-		.pipe( concat('all.css') )
-		
-		// minify
-		.pipe(rename({ suffix: '.min' }) )
-		.pipe( minifycss() )
-		.pipe( gulp.dest( './public/githubpage/' ) )
-})
-// concat icomoon css
-gulp.task('css-ghp2', function(){
-	return gulp.src([ 
-			'./public/stylesheets/icomoon/*.css',
-		])
-		.pipe( concat('icon.css') )
-		
-		// minify
-		.pipe(rename({ suffix: '.min' }) )
-		.pipe( minifycss() )
-		.pipe( gulp.dest( './public/githubpage/' ) )
-})
+	// for animate.json (list of animations)
+	gulp.src( js + '/*.json' )
+		.pipe( gulp.dest( gh.jsDest ) );
+});
 
-gulp.task('css-ghp', ['css', 'css-ghp1', 'css-ghp2'] )
+// fonts
+gulp.task('gh-fonts', function(){
+	return gulp.src( fonts + '/*/*' )
+		.pipe( gulp.dest( gh.fontDest ) );
+});
+
+
+gulp.task('gh-page', ['gh-jade', 'scss', 'gh-css', 'gh-scripts', 'gh-fonts'], function(){
+	console.log( 'successfully updated github page' );
+});
+
