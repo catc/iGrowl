0b96cf36b4a9b159d91cc734bf21ca5554957acf
Catalin Covic <catalin.covic@gmail.com>
2014-10-07 21:29:19 -0400

    cleaned javascript to meet jslint standards

--- dist/js/igrowl.js
+++ dist/js/igrowl.js
@@ -9,17 +9,17 @@
 	var animStart = 'webkitAnimationStart oanimationstart MSAnimationStart animationstart',
 		animEnd = 'webkitAnimationEnd oanimationend MSAnimationEnd animationend',
 	
-		growlTemplate = '<div class="igrowl animated" role="alert"><div class="igrowl-text"></div><button class="igrowl-dismiss i-times"></button></div>'
+		growlTemplate = '<div class="igrowl animated" role="alert"><div class="igrowl-text"></div><button class="igrowl-dismiss i-times"></button></div>';
 
 
 	var iGrowl = function(options){
-		var options = $.extend(true, {}, $.iGrowl.prototype.defaults, options)
-		this.options = options
+		options = $.extend(true, {}, $.iGrowl.prototype.defaults, options);
+		this.options = options;
 		
-		this.template = setContent(options)
+		this.template = setContent(options);
 
-		render.call(this)
-		return this
+		render.call(this);
+		return this;
 	},
 
 	// builds notification (title, message, icon)
@@ -27,49 +27,53 @@
 		// if no title or message, throw error
 		if ( !options.title && !options.message ) throw new Error('You must enter at least a title or message.');
 
-		var template = $(growlTemplate)
+		var template = $(growlTemplate);
 
 		// small
 		if ( options.small ) { template.addClass('igrowl-small'); }
 
 		// type
-		template.addClass('igrowl-'+options.type)
+		template.addClass('igrowl-'+options.type);
 
 
 		// icon - set up function to determine if icon is 24/18/32px grid
 		if ( options.icon ) template.prepend('<div class="igrowl-icon i-'+ options.icon + '"></div>');
 
 		// title + message
-		if ( options.title ) template.find('.igrowl-text').prepend('<div class="igrowl-title">' + options.title + '</div>')
-		if ( options.message ) template.find('.igrowl-text').append('<div class="igrowl-message">' + options.message + '</div>')
+		if ( options.title ) template.find('.igrowl-text').prepend('<div class="igrowl-title">' + options.title + '</div>');
+		if ( options.message ) template.find('.igrowl-text').append('<div class="igrowl-message">' + options.message + '</div>');
 
 		// link
-		if ( options.link ){ template.addClass('igrowl-link').children('.igrowl-icon, .igrowl-text').wrapAll('<a href="' + options.link +'" target="_' + options.target + '" />') }
+		if ( options.link ){ template.addClass('igrowl-link').children('.igrowl-icon, .igrowl-text').wrapAll('<a href="' + options.link +'" target="_' + options.target + '" />'); }
 
-		template.attr('alert-placement', options.placement.x + ' ' + options.placement.y )
-		return template
+		template.attr('alert-placement', options.placement.x + ' ' + options.placement.y );
+		return template;
 	},
 
 	// sets css position and appends to body
 	render = function(){
 		var options = this.options,
-			template = this.template
+			template = this.template;
 
 		var last = $('.igrowl[alert-placement="' + options.placement.x + ' ' + options.placement.y +'"]').last(),
 			y = options.offset.y,
-			growl = this
+			growl = this;
 
 		// vertical alignment - place after last element of type (if it exists)
 		if ( last.length ) {
-			y = parseInt( last.css( options.placement.y ), 10) + last.outerHeight() + options.spacing
+			y = parseInt( last.css( options.placement.y ), 10) + last.outerHeight() + options.spacing;
 		}
-		template.css( options.placement.y, y )
+		template.css( options.placement.y, y );
 
 
 		// horizontal alignment
-		options.placement.x === "center" ? template.addClass('igrowl-center') : template.css( options.placement.x, options.offset.x )
+		if ( options.placement.x === "center" ) { 
+			template.addClass('igrowl-center'); 
+		}  else { 
+			template.css( options.placement.x, options.offset.x ); 
+		}
 
-		$('body').append(template)
+		$('body').append(template);
 
 		// add animation class - if enabled
 		if ( options.animation ) {
@@ -79,47 +83,47 @@
 					if ( typeof options.onShow === 'function' ) options.onShow();
 				})
 				.one(animEnd, function(e) {
-					controls.call(growl)
+					controls.call(growl);
 				});
 
 		} else {
-			controls.call(growl)
+			controls.call(growl);
 		}
 	},
 
 	// sets up auto-dismiss after delay, and dismiss button
 	controls = function(){
 		var options = this.options,
-			template = this.template
+			template = this.template;
 
 		// callback once alert is visible/animation complete
 		if ( typeof options.onShown === 'function' ) options.onShown();
 		
-		var growl = this
+		var growl = this;
 		
 		// after delay, dismiss alert
 		if ( options.delay > 0 ){
 			setTimeout( function(){
-				growl.dismiss()
+				growl.dismiss();
 			}, options.delay);
 		}
 
 		// set up dismiss button
 		template.find('.igrowl-dismiss').on('click', function(){
-			growl.dismiss()
-		})
+			growl.dismiss();
+		});
 	},
 
 	updatePosition = function(){
 		var options = this.options,
-			template = $(this.template)
+			template = $(this.template);
 
 		template.nextAll('.igrowl[alert-placement="' + options.placement.x + ' ' + options.placement.y +'"]').each(function(i, alert){
 			// sets y as: 	current - ( alert to be dismissed height + alert to be dismissed spacing)
-			var y = parseInt( $(this).css( options.placement.y ), 10) - template.outerHeight() - options.spacing
-			$(alert).css(options.placement.y, y)
-		})
-		template.remove()
+			var y = parseInt( $(this).css( options.placement.y ), 10) - template.outerHeight() - options.spacing;
+			$(alert).css(options.placement.y, y);
+		});
+		template.remove();
 	};
 
 
@@ -128,7 +132,7 @@
 		dismiss: function(){
 			var options = this.options,
 				template = this.template,
-				growl = this
+				growl = this;
 
 			if ( options.animation ) {
 				this.template
@@ -139,19 +143,19 @@
 					})
 					.one(animEnd, function(e){
 						if ( typeof options.onHidden === 'function' ) options.onHidden();
-						updatePosition.call(growl)
-					})
+						updatePosition.call(growl);
+					});
 
 				// fallback in case animation event listener fails
 				setTimeout( function(){
-					template.hide()
-					updatePosition.call(growl)
-				}, 1500 )
+					template.hide();
+					updatePosition.call(growl);
+				}, 1500 );
 
 			} else {
-				template.hide()
+				template.hide();
 				if ( typeof options.onHidden === 'function' ) options.onHidden();
-				updatePosition.call(growl)
+				updatePosition.call(growl);
 			}
 
 		}
@@ -161,15 +165,15 @@
 	// initiate growl
 	$.iGrowl = function(settings){
 		// generate alert
-		var growl = new iGrowl( settings )
-		return growl
-	}
+		var growl = new iGrowl (settings);
+		return growl;
+	};
 
 	// dismiss all alerts
 	$.iGrowl.prototype.dismissAll = function(placement){
-		if ( placement === 'all' ) { $('.igrowl button').trigger('click') } 
-		else { $('.igrowl[alert-placement="'+placement+'"] button').trigger('click') }
-	}
+		if ( placement === 'all' ) { $('.igrowl button').trigger('click'); } 
+		else { $('.igrowl[alert-placement="'+placement+'"] button').trigger('click'); }
+	};
 
 	// default settings
 	$.iGrowl.prototype.defaults = {
@@ -200,7 +204,7 @@
 		onShown : 		null,
 		onHide : 		null,
 		onHidden : 		null,
-	}
+	};
 
 
 })( jQuery );
